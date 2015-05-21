c0b1ad217a5424faf3eb250c7f8e71ecca6df742
Bram Vogelaar <attachmentgenie@inuits.eu>
2015-05-21 14:28:41 +0200

    adding protocol option for foreman_proxy

--- manifests/profile_foreman_proxy.pp
+++ manifests/profile_foreman_proxy.pp
@@ -1,9 +1,10 @@
 class puppetmaster::profile_foreman_proxy (
   $foreman_host = 'foreman',
   $version      = 'present',
+  $protocol     = 'https',
 ) {
   class { '::foreman_proxy':
-    foreman_base_url => "http://${foreman_host}",
+    foreman_base_url => "${protocol}://${foreman_host}",
     trusted_hosts    => [$::fqdn, $foreman_host],
     bmc              => false,
     dhcp             => false,
